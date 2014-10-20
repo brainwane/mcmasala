d85748b9b886742ae2fe042f0638615a641fff96
Sumana Harihareswara <sumanah@wikimedia.org>
2014-10-20 13:39:54 -0400

    headline

--- parsearticles.py
+++ parsearticles.py
@@ -10,18 +10,16 @@ def parse_file():
     articles = soup.find_all("div", attrs={"class": "article"})
     article_list = []
     for article in articles:
-        # append it to the list
-        pass
+        article_list.append(parse_article(article))
     return article_dict # JSON time!
 
 def parse_article(element):
     article_data = {}
-    docid_string = element.find_all("p", text=re.compile("Document"))
+    docid_string = element.find("p", text=re.compile("Document"))
     # do a regex to find the actual OKLD.... string; add to dictionary
-    headline_string = element.find_all("div", id="hd")
-    # use BS to find the string in the child "b" tag here; add to dictionary
+    article_data["headline"] = element.find("div", id="hd").contents[0].contents[0]
     # text of article is after "all rights reserved" and before the Document ID
-
+    return article_data
 
 if __name__ == "__main__":
     parse_file()

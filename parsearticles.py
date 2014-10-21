9393c70297773be8c71e38b9ae3d4d1e98165cae
Sumana Harihareswara <sumanah@wikimedia.org>
2014-10-20 22:31:26 -0400

    Leonard comments on better using bs4

--- parsearticles.py
+++ parsearticles.py
@@ -26,7 +26,9 @@ def parse_article(element):
     article_data = {}
     article_data["doc_id"] = element.find("p", text=re.compile("Document")).contents[0].strip("Document ")
     article_data["headline"] = element.find("div", id="hd").contents[0].contents[0]
+    # instead of contents[0] try .text or .string
     if element.find("div", text=re.compile("The Oakland Tribune")):
+        # consider setting that element.find to a variable -- if not None, look at its .previous_element
         date = element.find("div", text=re.compile("The Oakland Tribune")).previous_element
         article_data["date"] = parse(date)
         wordcountstring = element.find("div", text=re.compile("The Oakland Tribune")).previous_sibling.previous_sibling.contents[0]
@@ -34,7 +36,9 @@ def parse_article(element):
     article_text = ""
     if element.find("div", text=re.compile("All rights reserved")):
         rights_tag = element.find("div", text=re.compile("All rights reserved"))
+        # assign it to rights_tag earlier. can use    if rights_tag
         for paragraph in rights_tag.next_element.next_element.next_siblings:
+            # maybe try find_next or find_next_siblings if it is all the <p> tag
             article_text = article_text + repr(paragraph)
             # text of article is after "all rights reserved" and before the Document ID
         article_text = article_text.rpartition("<p>Document OKLD")[0]

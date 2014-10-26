29171f83c116b422bbbf77d64cc43361eaffe6f3
Sumana Harihareswara <sumanah@panix.com>
2014-10-26 15:47:35 -0400

    WIP

--- parsearticles.py
+++ parsearticles.py
@@ -13,18 +13,28 @@ from bs4 import BeautifulSoup, UnicodeDammit
 from os import path
 from dateutil.parser import parse
 
-ARCHIVEFILE = "../../Documents/factiva/all-articles.htm"
+ARCHIVEFILES = ["../../Documents/mcm-archive/all-articles.htm", 
+"/home/sumanah/Documents/mcm-archive/leftovers/2005-05-26.html", 
+"/home/sumanah/Documents/mcm-archive/leftovers/2005-07-28.html", 
+"/home/sumanah/Documents/mcm-archive/leftovers/2005-08-04.html", 
+"/home/sumanah/Documents/mcm-archive/leftovers/2005-08-18.html", 
+"/home/sumanah/Documents/mcm-archive/leftovers/2005-10-09.html", 
+"/home/sumanah/Documents/mcm-archive/leftovers/2005-11-06.html", 
+"/home/sumanah/Documents/mcm-archive/leftovers/2005-11-20.html", 
+"/home/sumanah/Documents/mcm-archive/leftovers/2006-09-10.html", 
+"/home/sumanah/Documents/mcm-archive/leftovers/2007-05-20.html", 
+"/home/sumanah/Documents/mcm-archive/leftovers/2007-06-10.html"]
 
 
-def parse_article(element):
+
+def parse_archival_article(element):
+    ''' I concatenated together a big HTML file of a bunch of my columns.
+    This function uses Beautiful Soup to parse it for headline/body/date.'''
     article_data = {}
-    article_data["doc_id"] = element.find("p", text=re.compile("Document")).text.strip("Document ")
     article_data["headline"] = element.find("div", id = "hd").text.strip("\n").rstrip()
     trib = element.find("div", text=re.compile("The Oakland Tribune"))
     if trib:
         article_data["date"] = trib.previous_element
-        wordcountstring = trib.previous_sibling.previous_sibling.text
-        article_data["wordcount"] = int(wordcountstring.strip(" words"))
     article_text = ""
     rights_tag = element.find("div", text=re.compile("All rights reserved"))
     if rights_tag:
@@ -38,19 +48,45 @@ def parse_article(element):
         print(article_data["headline"]+" has no rightstag")
     return article_data
 
-def parse_file(filename, article_list):
+
+def parse_leftover_article(soup):
+    '''I also grabbed some additional articles from another site.
+    They don't have the DocIDs or wordcounts.'''
+    article_data = {}
+    postdate = soup.find("div", attrs={"class":"articleDate"})
+    article_data["date"] = repr(postdate)
+    article_data["headline"] = soup.find("meta", attrs={"property": "twitter:title"})["content"]
+    body = soup.find("div", id = "articleViewerGroup").previous_element.contents
+    prose = ""
+    for para in body[3:]:
+        prose += repr(para)
+    article_data["body"] = prose
+    return article_data
+
+
+def parse_files(filenames, article_list):
     '''Returns a date-sorted list of dictionaries, and a date-sorted list
     of dates-and-headlines dicts.'''
-    file_path = path.relpath(filename)
-    with open(file_path) as f:
-        newsfile = f.read()
-    cols = UnicodeDammit.detwingle(newsfile)
-    soup = BeautifulSoup(cols, "html5lib")
-    articles = soup.find_all("div", attrs={"class": "article"})
-    for article in articles:
-        headline = article.find("div", id = "hd").text.strip("\n").rstrip()
-        if is_unique(headline, article_list):
-            article_list.append(parse_article(article))
+    print("running")
+    for filename in filenames:
+        file_path = path.relpath(filename)
+        print(file_path)
+        with open(file_path) as f:
+            newsfile = f.read()
+        if ".html" in file_path:
+            soup = BeautifulSoup(newsfile, "html5lib")
+        else:
+            cols = UnicodeDammit.detwingle(newsfile)
+            soup = BeautifulSoup(cols, "html5lib")
+        archival_articles = soup.find_all("div", attrs={"class": "article"})
+        for article in archival_articles:
+            headline = article.find("div", id = "hd").text.strip("\n").rstrip()
+            if is_unique(headline, article_list):
+                article_list.append(parse_archival_article(article))
+        if archival_articles == []:
+            headline = soup.find("meta", attrs={"property": "twitter:title"})["content"]
+            if is_unique(headline, article_list):
+                article_list.append(parse_leftover_article(soup))
     article_list.sort(key=lambda k: parse(k["date"]))
     index = [{k:v for (k,v) in story.items() if ("date" in k) or ("headline" in k)} for story in article_list]
     return (article_list, index)
@@ -63,4 +99,4 @@ def is_unique(headline, article_list):
     return True
 
 if __name__ == "__main__":
-    parse_file(ARCHIVEFILE, [])
+    parse_files(ARCHIVEFILES, [])
